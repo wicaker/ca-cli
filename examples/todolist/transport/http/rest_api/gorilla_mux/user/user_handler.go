da7dd8fed21f5dd46e47717a6e365e15bba81bfb
wicaker <wicaksanaigede@gmail.com>
2019-12-26 17:46:03 +0800

    initiate http transport with gorilla/mux server

---
+++ examples/todolist/transport/http/rest_api/gorilla_mux/user/user_handler.go
@@ -0,0 +1,90 @@
+package userhandler
+
+import (
+	"context"
+	"net/http"
+	"todolist/domain"
+	"todolist/middleware"
+
+	"github.com/gorilla/mux"
+	json "github.com/json-iterator/go"
+)
+
+// UserHandler represent the httphandler for user
+type UserHandler struct {
+	UserUsecase domain.UserUsecase
+}
+
+// NewUserHandler will initialize the user endpoint
+func NewUserHandler(r *mux.Router, u domain.UserUsecase) {
+	handler := &UserHandler{
+		UserUsecase: u,
+	}
+
+	r.HandleFunc("/user/register", handler.Register).Methods("POST")
+	r.HandleFunc("/user/login", handler.Login).Methods("POST")
+}
+
+// Register will handle register request
+func (uh *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
+	var user domain.User
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(&domain.ResponseError{Message: err.Error()})
+		return
+	}
+	if ok, err := middleware.IsRequestValid(&user); !ok {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(&domain.ResponseError{Message: err.Error()})
+		return
+	}
+
+	ctx := context.Background()
+
+	if err := uh.UserUsecase.Register(ctx, &user); err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(&domain.ResponseError{Message: err.Error()})
+		return
+	}
+
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(&domain.ResponseSuccess{Message: "Successfully register new user"})
+	return
+}
+
+// Login will handle login request
+func (uh *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var user domain.User
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(&domain.ResponseError{Message: err.Error()})
+		return
+	}
+	if ok, err := middleware.IsRequestValid(&user); !ok {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(&domain.ResponseError{Message: err.Error()})
+		return
+	}
+
+	ctx := context.Background()
+	type data struct {
+		Token string `json:"token"`
+	}
+	d := &data{}
+	d.Token, err = uh.UserUsecase.Login(ctx, &user)
+
+	if err != nil {
+		w.WriteHeader(401)
+		json.NewEncoder(w).Encode(&domain.ResponseError{Message: err.Error()})
+		return
+	}
+
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(&domain.ResponseSuccess{Message: "Login successfully", Data: d})
+	return
+}
