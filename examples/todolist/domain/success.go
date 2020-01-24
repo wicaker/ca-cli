2bfca5407086aba21a83620f8ea93e0aeff11cf8
wicaker <wicaksanaigede@gmail.com>
2020-01-24 09:31:20 +0800

    fix typo

--- examples/todolist/domain/success.go
+++ examples/todolist/domain/success.go
@@ -1,6 +1,6 @@
 package domain
 
-// ResponseSuccess represent the reseponse error struct
+// ResponseSuccess represent the reseponse success struct
 type ResponseSuccess struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
