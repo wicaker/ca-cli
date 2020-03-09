7acea9e3c7a18d3e6efdaf2cba31638fdf06c1f7
wicaker <wicaksanaigede@gmail.com>
2020-03-09 18:59:33 +0800

    test database_config generator

--- generator/gen_database_config.go
+++ generator/gen_database_config.go
@@ -2,58 +2,59 @@ package generator
 
 import (
 	"github.com/dave/jennifer/jen"
-	"github.com/wicaker/cacli/domain"
 )
 
-func (gen *caGen) GenGopgConfig(dirName string, domainName string, gomodName string, parser *domain.Parser) error {
+func (gen *caGen) GenGopgConfig(dirName string) error {
 	f := jen.NewFile("config")
 	f.ImportAlias("github.com/go-pg/pg/v9", "pg")
 
 	f.Comment("GopgInit will connecting service to databsase using go-pg orm")
 	f.Func().Id("GopgInit").Params().Op("*").Qual("github.com/go-pg/pg/v9", "DB").Block(
 		jen.Id("db").Op(":=").Qual("github.com/go-pg/pg/v9", "Connect").Call(jen.Op("&").Qual("github.com/go-pg/pg/v9", "Options").Values(jen.Dict{
-			jen.Id("User"):     jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_USER")),
-			jen.Id("Database"): jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_NAME")),
-			jen.Id("Password"): jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_PASSWORD")),
+			jen.Id("Addr"):     jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_HOST_PG")).Op("+").Lit(":").Op("+").Qual("os", "Getenv").Call(jen.Lit("DATABASE_PORT_PG")),
+			jen.Id("Database"): jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_NAME_PG")),
+			jen.Id("Password"): jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_PASSWORD_PG")),
+			jen.Id("User"):     jen.Qual("os", "Getenv").Call(jen.Lit("DATABASE_USER_PG")),
 		})),
+		jen.Line(),
 		jen.Return(jen.Id("db")),
 	)
 
-	err := f.Save(dirName + "/database/config/gopg_config.go")
+	err := f.Save(dirName + "/gopg_config.go")
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (gen *caGen) GenGormConfig(dirName string, domainName string, gomodName string, parser *domain.Parser) error {
+func (gen *caGen) GenGormConfig(dirName string) error {
 	f := jen.NewFile("config")
 
 	f.Comment("GormInit will connecting service to databsase using GORM orm")
 	f.Func().Id("GormInit").Params().Op("*").Qual("github.com/jinzhu/gorm", "DB").Block(
 		jen.Id("dbHost").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_HOST")),
 		jen.Id("dbUser").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_USER")),
-		jen.Id("dbName").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_NAME")),
 		jen.Id("dbPassword").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_PASSWORD")),
-
+		jen.Id("dbName").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_NAME")),
+		jen.Line(),
 		jen.Id("dsn").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("host=%s user=%s dbname=%s sslmode=disable password=%s"), jen.Id("dbHost"), jen.Id("dbUser"), jen.Id("dbName"), jen.Id("dbPassword")),
-
+		jen.Line(),
 		jen.List(jen.Id("db"), jen.Err()).Op(":=").Qual("github.com/jinzhu/gorm", "Open").Call(jen.Lit("postgres"), jen.Id("dsn")),
 		jen.If(jen.Err().Op("!=").Nil()).Block(
 			jen.Panic(jen.Lit("failed to connect to database")),
 		),
-
+		jen.Line(),
 		jen.Return(jen.Id("db")),
 	)
 
-	err := f.Save(dirName + "/database/config/gorm_config.go")
+	err := f.Save(dirName + "/gorm_config.go")
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (gen *caGen) GenSQLConfig(dirName string, domainName string, gomodName string, parser *domain.Parser) error {
+func (gen *caGen) GenSQLConfig(dirName string) error {
 	f := jen.NewFile("config")
 
 	f.Comment("SQLInit will connecting service to databsase using database/sql standard library")
@@ -63,31 +64,28 @@ func (gen *caGen) GenSQLConfig(dirName string, domainName string, gomodName stri
 		jen.Id("dbUser").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_USER")),
 		jen.Id("dbPass").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_PASSWORD")),
 		jen.Id("dbName").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_NAME")),
-
+		jen.Line(),
 		jen.Id("connection").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("%s:%s@tcp(%s:%s)/%s"), jen.Id("dbUser"), jen.Id("dbPass"), jen.Id("dbHost"), jen.Id("dbPort"), jen.Id("dbName")),
-
 		jen.Id("val").Op(":=").Qual("net/url", "Values").Op("{}"),
-		jen.Qual("val", "Add").Call(jen.Lit("parseTime"), jen.Lit("1")),
-		jen.Qual("val", "Add").Call(jen.Lit("loc"), jen.Lit("Asia/Jakarta")),
-
-		jen.Id("dsn").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("%s?%s"), jen.Id("connection"), jen.Qual("val", "Encode").Op("()")),
-		// dbConn, err := sql.Open(`mysql`, dsn)
+		jen.Id("val").Dot("Add").Call(jen.Lit("parseTime"), jen.Lit("1")),
+		jen.Id("val").Dot("Add").Call(jen.Lit("loc"), jen.Lit("Asia/Jakarta")),
+		jen.Id("dsn").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("%s?%s"), jen.Id("connection"), jen.Id("val").Dot("Encode").Op("()")),
 		jen.List(jen.Id("dbConn"), jen.Err()).Op(":=").Qual("database/sql", "Open").Call(jen.Lit("mysql"), jen.Id("dsn")),
 		jen.If(jen.Err().Op("!=").Nil()).Block(
-			jen.Panic(jen.Lit("failed to connect to database")),
+			jen.Panic(jen.Err()),
 		),
-
+		jen.Line(),
 		jen.Return(jen.Id("dbConn")),
 	)
 
-	err := f.Save(dirName + "/database/config/sql_config.go")
+	err := f.Save(dirName + "/sql_config.go")
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (gen *caGen) GenSqlxConfig(dirName string, domainName string, gomodName string, parser *domain.Parser) error {
+func (gen *caGen) GenSqlxConfig(dirName string) error {
 	f := jen.NewFile("config")
 
 	f.Comment("SqlxInit will connecting service to databsase using sqlx library")
@@ -97,24 +95,21 @@ func (gen *caGen) GenSqlxConfig(dirName string, domainName string, gomodName str
 		jen.Id("dbUser").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_USER")),
 		jen.Id("dbPass").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_PASSWORD")),
 		jen.Id("dbName").Op(":=").Qual("os", "Getenv").Call(jen.Lit("DATABASE_NAME")),
-
+		jen.Line(),
 		jen.Id("connection").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("%s:%s@tcp(%s:%s)/%s"), jen.Id("dbUser"), jen.Id("dbPass"), jen.Id("dbHost"), jen.Id("dbPort"), jen.Id("dbName")),
-
 		jen.Id("val").Op(":=").Qual("net/url", "Values").Op("{}"),
-		jen.Qual("val", "Add").Call(jen.Lit("parseTime"), jen.Lit("1")),
-		jen.Qual("val", "Add").Call(jen.Lit("loc"), jen.Lit("Asia/Jakarta")),
-
-		jen.Id("dsn").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("%s?%s"), jen.Id("connection"), jen.Qual("val", "Encode").Op("()")),
-		// dbConn, err := sql.Open(`mysql`, dsn)
+		jen.Id("val").Dot("Add").Call(jen.Lit("parseTime"), jen.Lit("1")),
+		jen.Id("val").Dot("Add").Call(jen.Lit("loc"), jen.Lit("Asia/Jakarta")),
+		jen.Id("dsn").Op(":=").Qual("fmt", "Sprintf").Call(jen.Lit("%s?%s"), jen.Id("connection"), jen.Id("val").Dot("Encode").Op("()")),
 		jen.List(jen.Id("dbConn"), jen.Err()).Op(":=").Qual("github.com/jmoiron/sqlx", "Connect").Call(jen.Lit("mysql"), jen.Id("dsn")),
 		jen.If(jen.Err().Op("!=").Nil()).Block(
-			jen.Panic(jen.Lit("failed to connect to database")),
+			jen.Panic(jen.Err()),
 		),
-
+		jen.Line(),
 		jen.Return(jen.Id("dbConn")),
 	)
 
-	err := f.Save(dirName + "/database/config/sqlx_config.go")
+	err := f.Save(dirName + "/sqlx_config.go")
 	if err != nil {
 		return err
 	}
---
+++ generator/gen_database_config_test.go
@@ -0,0 +1,363 @@
+package generator_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/wicaker/cacli/fs"
+	"github.com/wicaker/cacli/generator"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	expected_gopg_config = `package config
+
+import (
+	pg "github.com/go-pg/pg/v9"
+	"os"
+)
+
+// GopgInit will connecting service to databsase using go-pg orm
+func GopgInit() *pg.DB {
+	db := pg.Connect(&pg.Options{
+		Addr:     os.Getenv("DATABASE_HOST_PG") + ":" + os.Getenv("DATABASE_PORT_PG"),
+		Database: os.Getenv("DATABASE_NAME_PG"),
+		Password: os.Getenv("DATABASE_PASSWORD_PG"),
+		User:     os.Getenv("DATABASE_USER_PG"),
+	})
+
+	return db
+}
+`
+	expected_gorm_config = `package config
+
+import (
+	"fmt"
+	gorm "github.com/jinzhu/gorm"
+	"os"
+)
+
+// GormInit will connecting service to databsase using GORM orm
+func GormInit() *gorm.DB {
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbUser, dbName, dbPassword)
+
+	db, err := gorm.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	return db
+}
+`
+
+	expected_sql_config = `package config
+
+import (
+	"database/sql"
+	"fmt"
+	"net/url"
+	"os"
+)
+
+// SQLInit will connecting service to databsase using database/sql standard library
+func SQLInit() *sql.DB {
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	return dbConn
+}
+`
+
+	expected_sqlx_config = `package config
+
+import (
+	"fmt"
+	sqlx "github.com/jmoiron/sqlx"
+	"net/url"
+	"os"
+)
+
+// SqlxInit will connecting service to databsase using sqlx library
+func SqlxInit() *sqlx.DB {
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	return dbConn
+}
+`
+)
+
+func TestGenerateGopgConfig(t *testing.T) {
+	var (
+		serviceName = "test_gopg_config"
+		dirDb       = "database"
+		dirConf     = "config"
+		dirName     = fmt.Sprintf("%s/%s/%s", serviceName, dirDb, dirConf)
+		newFs       = fs.NewFsService()
+	)
+
+	t.Run("success, should generate an gopg_config.go file", func(t *testing.T) {
+		// create directory of service
+		err := newFs.CreateDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb + "/" + dirConf)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		// generate gopg_config.go file
+		gen := generator.NewGeneratorService()
+		err = gen.GenGopgConfig(dirName)
+		resGopg, err := newFs.FindFile(dirName + "/gopg_config.go")
+		assert.NoError(t, err)
+		assert.NotEqual(t, nil, resGopg)
+
+		data, err := ioutil.ReadFile(dirName + "/gopg_config.go")
+		if err != nil {
+			log.Error("File reading error", err)
+			os.Exit(1)
+		}
+		assert.Equal(t, expected_gopg_config, string(data))
+
+		// remove directory of service
+		err = newFs.RemoveDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+	})
+
+	t.Run("failed, because directory not found", func(t *testing.T) {
+		// generate gopg_config file
+		gen := generator.NewGeneratorService()
+		err := gen.GenGopgConfig(serviceName)
+
+		assert.Error(t, err)
+	})
+}
+
+func TestGenerateGormConfig(t *testing.T) {
+	var (
+		serviceName = "test_gorm_config"
+		dirDb       = "database"
+		dirConf     = "config"
+		dirName     = fmt.Sprintf("%s/%s/%s", serviceName, dirDb, dirConf)
+		newFs       = fs.NewFsService()
+	)
+
+	t.Run("success, should generate an gorm_config.go file", func(t *testing.T) {
+		// create directory of service
+		err := newFs.CreateDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb + "/" + dirConf)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		// generate gorm_config.go file
+		gen := generator.NewGeneratorService()
+		err = gen.GenGormConfig(dirName)
+		resGorm, err := newFs.FindFile(dirName + "/gorm_config.go")
+		assert.NoError(t, err)
+		assert.NotEqual(t, nil, resGorm)
+
+		data, err := ioutil.ReadFile(dirName + "/gorm_config.go")
+		if err != nil {
+			log.Error("File reading error", err)
+			os.Exit(1)
+		}
+		assert.Equal(t, expected_gorm_config, string(data))
+
+		// remove directory of service
+		err = newFs.RemoveDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+	})
+
+	t.Run("failed, because directory not found", func(t *testing.T) {
+		// generate gorm_config file
+		gen := generator.NewGeneratorService()
+		err := gen.GenGopgConfig(serviceName)
+
+		assert.Error(t, err)
+	})
+}
+
+func TestGenerateSQLConfig(t *testing.T) {
+	var (
+		serviceName = "test_sql_config"
+		dirDb       = "database"
+		dirConf     = "config"
+		dirName     = fmt.Sprintf("%s/%s/%s", serviceName, dirDb, dirConf)
+		newFs       = fs.NewFsService()
+	)
+
+	t.Run("success, should generate an sql_config.go file", func(t *testing.T) {
+		// create directory of service
+		err := newFs.CreateDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb + "/" + dirConf)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		// generate sql_config.go file
+		gen := generator.NewGeneratorService()
+		err = gen.GenSQLConfig(dirName)
+		resSql, err := newFs.FindFile(dirName + "/sql_config.go")
+		assert.NoError(t, err)
+		assert.NotEqual(t, nil, resSql)
+
+		data, err := ioutil.ReadFile(dirName + "/sql_config.go")
+		if err != nil {
+			log.Error("File reading error", err)
+			os.Exit(1)
+		}
+		assert.Equal(t, expected_sql_config, string(data))
+
+		// remove directory of service
+		err = newFs.RemoveDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+	})
+
+	t.Run("failed, because directory not found", func(t *testing.T) {
+		// generate sql_config file
+		gen := generator.NewGeneratorService()
+		err := gen.GenGopgConfig(serviceName)
+
+		assert.Error(t, err)
+	})
+}
+
+func TestGenerateSqlxConfig(t *testing.T) {
+	var (
+		serviceName = "test_sqlx_config"
+		dirDb       = "database"
+		dirConf     = "config"
+		dirName     = fmt.Sprintf("%s/%s/%s", serviceName, dirDb, dirConf)
+		newFs       = fs.NewFsService()
+	)
+
+	t.Run("success, should generate an sqlx_config.go file", func(t *testing.T) {
+		// create directory of service
+		err := newFs.CreateDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		err = newFs.CreateDir(serviceName + "/" + dirDb + "/" + dirConf)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		// generate sqlx_config.go file
+		gen := generator.NewGeneratorService()
+		err = gen.GenSqlxConfig(dirName)
+		resSqlx, err := newFs.FindFile(dirName + "/sqlx_config.go")
+		assert.NoError(t, err)
+		assert.NotEqual(t, nil, resSqlx)
+
+		data, err := ioutil.ReadFile(dirName + "/sqlx_config.go")
+		if err != nil {
+			log.Error("File reading error", err)
+			os.Exit(1)
+		}
+		assert.Equal(t, expected_sqlx_config, string(data))
+
+		// remove directory of service
+		err = newFs.RemoveDir(serviceName)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+	})
+
+	t.Run("failed, because directory not found", func(t *testing.T) {
+		// generate sqlx_config file
+		gen := generator.NewGeneratorService()
+		err := gen.GenGopgConfig(serviceName)
+
+		assert.Error(t, err)
+	})
+}
