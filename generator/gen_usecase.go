bb6f348c2edc237ace2095a29eb48be58527af97
wicaker <wicaksanaigede@gmail.com>
2020-03-15 13:04:42 +0800

    implement new reusable code trough function

--- generator/gen_usecase.go
+++ generator/gen_usecase.go
@@ -52,40 +52,14 @@ func (gen *caGen) GenUsecase(dirName string, domainFile string, gomodName string
 	)
 
 	for _, i := range parser.Usecase.Method {
-		var param []jen.Code
-		var result []jen.Code
-		var returnV []jen.Code
-		for _, j := range i.ParameterList {
-			param = append(param, jen.Id(j.Name).Op(j.Type))
-		}
-		for _, k := range i.ResultList {
-			result = append(result, jen.Id(k.Name).Op(k.Type))
-			switch k.Type {
-			case "string":
-				returnV = append(returnV, jen.Op(`""`))
-			case "bool":
-				returnV = append(returnV, jen.Op("false"))
-			case "float32", "float64", "complex64", "complex128":
-				returnV = append(returnV, jen.Op("0.0"))
-			case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64", "uintpr", "byte", "rune":
-				returnV = append(returnV, jen.Op("0"))
-			default:
-				if len(k.Type) > 7 {
-					if k.Type[:7] == "domain." {
-						returnV = append(returnV, jen.Op(k.Type+"{}"))
-					} else {
-						returnV = append(returnV, jen.Nil())
-					}
-				} else {
-					returnV = append(returnV, jen.Nil())
-				}
-			}
-
-		}
+		var (
+			param            = genParamList(i)
+			returnT, returnV = genReturnList(i)
+		)
 		f.Line()
 		f.Func().
 			Params(jen.Id(string(domainName[0])+"u").Op("*").Id(domainName+"Usecase")).
-			Id(i.Name).Params(param[:]...).Call(result[:]...).Block(
+			Id(i.Name).Params(param[:]...).Call(returnT[:]...).Block(
 			jen.List(jen.Id("ctx"), jen.Id("cancel")).Op(":=").Qual("context", "WithTimeout").Call(jen.Id("ctx"), jen.Id(string(domainName[0])+"u").Dot("contextTimeout")),
 			jen.Id("defer").Id("cancel").Call(),
 			jen.Return(returnV[:]...),
--- generator/generator.go
+++ generator/generator.go
@@ -4,6 +4,7 @@ Package generator used to generate codes and create a file based on the layer
 package generator
 
 import (
+	"github.com/dave/jennifer/jen"
 	"github.com/wicaker/cacli/domain"
 )
 
@@ -14,4 +15,39 @@ type caGen struct {
 // NewGeneratorService will create new a caGen object representation of domain.Generator interface
 func NewGeneratorService() domain.GeneratorService {
 	return &caGen{}
+}
+
+func genParamList(i domain.Method) []jen.Code {
+	var param []jen.Code
+	for _, j := range i.ParameterList {
+		param = append(param, jen.Id(j.Name).Op(j.Type))
+	}
+	return param
+}
+
+func genReturnList(i domain.Method) (returnType []jen.Code, returnValue []jen.Code) {
+	for _, k := range i.ResultList {
+		returnType = append(returnType, jen.Id(k.Name).Op(k.Type))
+		switch k.Type {
+		case "string":
+			returnValue = append(returnValue, jen.Op(`""`))
+		case "bool":
+			returnValue = append(returnValue, jen.Op("false"))
+		case "float32", "float64", "complex64", "complex128":
+			returnValue = append(returnValue, jen.Op("0.0"))
+		case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64", "uintpr", "byte", "rune":
+			returnValue = append(returnValue, jen.Op("0"))
+		default:
+			if len(k.Type) > 7 {
+				if k.Type[:7] == "domain." {
+					returnValue = append(returnValue, jen.Op(k.Type+"{}"))
+				} else {
+					returnValue = append(returnValue, jen.Nil())
+				}
+			} else {
+				returnValue = append(returnValue, jen.Nil())
+			}
+		}
+	}
+	return
 }
